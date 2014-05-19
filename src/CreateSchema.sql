31210caa250762be5cfe16cd7915e9de6306607b
JO <socialmedia@nododos.com>
2014-05-19 11:02:02 -0500

    Added second table

--- src/CreateSchema.sql
+++ src/CreateSchema.sql
@@ -5,3 +5,12 @@ create table followers
     screen_name varchar2(50),
     follower varchar2(50)
 )
+
+create table following
+(
+    id integer,
+    tweet_date varchar2(100),
+    screen_name varchar2(50),
+    followingId integer,	
+    following varchar2(50)
+)
